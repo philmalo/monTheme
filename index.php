c6f99abe3bd0d23ca6c24a0c02aa5b7c5b7678b7
Cyclonicks <malo.philippe2021@gmail.com>
2023-02-09 15:45:26 -0500

    c4 exemples the_content, the_excerpt et wp_trim_words

--- index.php
+++ index.php
@@ -6,14 +6,18 @@ get_header()
 ?>
 <main>
     <h3>index.php</h3>
-<?php 
-if (have_posts()):
-    while(have_posts()): the_post();
-        the_title('<h1>', '</h1>');
-    endwhile;
-endif;
-?>
+    <?php 
+    if (have_posts()):
+        while(have_posts()): the_post();
+            the_title('<h1>', '</h1>');
+            //the_content(); //le contenu
+            //the_excerpt(); // un résumé
+            echo wp_trim_words( get_the_excerpt(), 4); //wp_trim_words requiert un echo, get_the excerpt fait un return, the_excerpt fait un echo,d'où le get_the_excerpt
+        endwhile;
+    endif;
+    ?>
 </main>
-<?php get_footer() 
 
+<?php
+get_footer()
 ?>
