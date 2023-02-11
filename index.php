c28509cb708e0d57bfb5ffa5c5ababc9ef97054c
Cyclonicks <malo.philippe2021@gmail.com>
2023-02-11 00:32:41 -0500

    hc tentative d'ajout de classes

--- index.php
+++ index.php
@@ -11,12 +11,19 @@ get_header()
         while(have_posts()): the_post();
             //the_title('<h1>', '</h1>');
             //the_permalink();?>
-            <h1><a href="<?php the_permalink();?>"><?= get_the_title()?></a></h1>
-            <?php
-            //the_content(); //le contenu
-            //the_excerpt(); // un résumé
-            echo wp_trim_words( get_the_excerpt(), 4); //wp_trim_words requiert un echo car il retourne une chaîne de caractères, get_the_excerpt fait un return, the_excerpt fait un echo par lui-même,d'où le get_the_excerpt
-        endwhile;
+            <section class="informations">
+            <h1>
+                <a href="<?php the_permalink();?>">
+            <?= get_the_title()?>
+                </a>
+            </h1>
+            
+            <!-- the_content(); le contenu
+            the_excerpt(); un résumé -->
+            <?php echo wp_trim_words( get_the_excerpt(), 4);?>
+            <!-- wp_trim_words requiert un echo car il retourne une chaîne de caractères, get_the_excerpt fait un return, the_excerpt fait un echo par lui-même,d'où le get_the_excerpt -->
+            </section>
+        <?php endwhile;
     endif;
     ?>
 </main>
--- single.php
+++ single.php
@@ -5,9 +5,14 @@ get_header()
     <code>single.php</code>
     <?php 
     if (have_posts()):
-        while(have_posts()): the_post();
-            the_title('<h1>', '</h1>');
-            the_content(); //le contenu
+        while(have_posts()): the_post();?>
+        <article class="carte-information">
+            <?php the_title('<h1>', '</h1>');?>
+            <div>
+                <?php the_content(); //le contenu ?>
+            </div>
+        </article>
+        <?php
         endwhile;
     endif;
     ?>
