e3651c2cdeb3e4fb69f2c9edacd6c39cc176f2b3
Cyclonicks <malo.philippe2021@gmail.com>
2023-02-24 00:26:42 -0500

    homecode ajustement layout fichiers markdown

--- front-page.php
+++ front-page.php
@@ -10,7 +10,7 @@ get_header()
             <article>
                 <h3><?= get_the_title();?></h3>
             
-                <p><?php echo wp_trim_words( get_the_excerpt(), 4);?></p>
+                <p><?php echo wp_trim_words( get_the_excerpt(), 15);?></p>
                 <a href="<?php the_permalink();?>">En savoir plus&#9758;</a>
                 <!-- wp_trim_words requiert un echo car il retourne une chaîne de caractères, get_the_excerpt fait un return, the_excerpt fait un echo par lui-même,d'où le get_the_excerpt -->
             </article>
--- single.php
+++ single.php
@@ -3,19 +3,20 @@ get_header()
 ?>
 <main>
     <code>single.php</code>
-    <?php 
-    if (have_posts()):
-        while(have_posts()): the_post();?>
-        <article class="carte-information">
-            <?php the_title('<h1>', '</h1>');?>
-            <div>
-                <?php the_content(); //le contenu ?>
-            </div>
-        </article>
-        <?php
-        endwhile;
-    endif;
-    ?>
+    <section class="item-unitaire">
+        <?php if (have_posts()):
+            while(have_posts()): the_post();?>
+            <article class="carte-information">
+                <?php the_title('<h1>', '</h1>');?>
+                <div>
+                    <?php the_content(); //le contenu ?>
+                </div>
+            </article>
+            <?php
+            endwhile;
+        endif;
+        ?>
+    </section>
 </main>
 
 <?php
