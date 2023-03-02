f1a7d231ae50c7533999305b1e8e7698cf41b89c
Cyclonicks <malo.philippe2021@gmail.com>
2023-03-02 16:58:06 -0500

    c10 finalisation affichage galerie

--- front-page.php
+++ front-page.php
@@ -7,8 +7,13 @@ get_header()
     <section class="liste-articles">
     <?php if (have_posts()):
         while(have_posts()): the_post();?>
-        <?php get_template_part("template-parts/categorie", "notes-wp");?>
-        <?php endwhile;?>
+        <?php if(in_category('galerie')){
+            get_template_part("template-parts/categorie", "galerie");
+        }
+        else{
+            get_template_part("template-parts/categorie", "notes-wp");
+        }
+        endwhile;?>
     <?php endif;?>
     </section>
 </main>
---
+++ template-parts/categorie-galerie.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * template-part qui permettra d'afficher un article de catégorie galerie dans la classe liste-articles
+     */
+?>
+
+<article class="galerie">
+    <?= the_content();?>
+</article>
