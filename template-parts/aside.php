1a53d593e44efbe51a78c0d0ef45cbb6c3a66684
Cyclonicks <malo.philippe2021@gmail.com>
2023-04-02 20:49:32 -0400

    c18 changement apparence template-evenement, ajustement affichage aside, disposition du footer selon les dimensions d'écrans

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -6,6 +6,9 @@
         if(in_category("cours")){
             $leMenu = "cours";
         }
+        if(is_page_template('template-evenement.php')){
+            $leMenu = "evenement";
+        }
     wp_nav_menu(array(
         "menu" => $leMenu,
         "container" => "nav"
